c82912089fa44c7e8aedea4c049007528c5955df
MetaEMK <Jan-Benecke@t-online.de>
2024-01-22 16:43:57 +0100

    bugfix: realtime functions

--- src/router/realtime/routes/passenger/passenger.go
+++ src/router/realtime/routes/passenger/passenger.go
@@ -1,10 +1,11 @@
 package passenger
 
 import (
+	"github.com/MetaEMK/FGK_PASMAS_backend/model"
 	"github.com/MetaEMK/FGK_PASMAS_backend/router/realtime"
 )
 
-func PublishPassengerEvent(actionType realtime.ActionType, data interface{}) {
+func PublishPassengerEvent(actionType realtime.ActionType, data *model.Passenger) {
     body := realtime.RealtimeBodyModel {
         Action: actionType,
         Data: data,
