2f6fd9f96115752fcdb75fa336c48faa3a11d03b
MetaEMK <Jan-Benecke@t-online.de>
2024-05-05 23:16:42 +0200

    bugfix realtime

--- src/databaseHandler/gormDivision.go
+++ src/databaseHandler/gormDivision.go
@@ -7,41 +7,39 @@ import (
 )
 
 func initDivision() {
-    Db.AutoMigrate(&model.Division{})
+	Db.AutoMigrate(&model.Division{})
 
-    defer SeedDivision()
+	defer SeedDivision()
 }
 
 func SeedDivision() error {
-    if !config.EnableSeeder {
-        return nil
-    }
+	if config.EnableSeeder {
+		log.Debug("Seeding divisions")
+		divs := []model.Division{
+			{Name: "Segelflug", PassengerCapacity: 1},
+			{Name: "Motorsegler", PassengerCapacity: 1},
+			{Name: "Motorflug", PassengerCapacity: 3},
+		}
 
-    log.Debug("Seeding divisions")
-    divs := []model.Division{
-        { Name: "Segelflug", PassengerCapacity: 1},
-        { Name: "Motorsegler", PassengerCapacity: 1},
-        { Name: "Motorflug", PassengerCapacity: 3},
-    }
+		for _, div := range divs {
+			d := model.Division{}
+			res := Db.Where("name = ?", div.Name).Find(&d)
 
-    for _, div := range divs {
-        d := model.Division{}
-        res := Db.Where("name = ?", div.Name).Find(&d)
+			if res.RowsAffected == 0 {
+				Db.Create(&div)
+			} else {
+				Db.Model(&d).Updates(div)
+			}
+		}
+	}
 
-        if res.RowsAffected == 0 {
-            Db.Create(&div)
-        } else {
-            Db.Model(&d).Updates(div)
-        }
-    }
+	divisions := []model.Division{}
+	err := Db.Find(&divisions).Error
+	if err != nil {
+		return err
+	}
 
-    divisions := []model.Division{}
-    err := Db.Find(&divisions).Error
-    if err != nil {
-        return err
-    }
+	realtime.InitAllFlightByDivisionEndpoints(divisions)
 
-    realtime.InitAllFlightByDivisionEndpoints(divisions)
-
-    return nil
+	return nil
 }
